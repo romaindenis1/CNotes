f5d66c4e36a459ea4dad583ca848a613b74f655b
Romain <163823947+romaindenis1@users.noreply.github.com>
2025-08-28 11:22:38 +0200

    Create(main.c): Setup of project

---
+++ main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int main ()
+{
+    printf("Welcome to notes app\n");
+    printf("1) Add a note\n");
+    printf("2) List notes\n");
+    printf("3) Search note\n");
+    printf("4) Delete a note\n");
+    printf("Que voulez vous faire? ");
+
+    //d pour double
+    scanf("%d", &userInput);
+    switch (userInput)
+    {
+        case 1:
+            addNote();
+            break;
+        case 2:
+            listNotes();
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+    }
+    return 0;
+}
+void addNote()
+{
+    char filename[50];
+    char extension[] = ".txt";
+    char content[255];
+
+    FILE *fptr;
+
+    printf("Comment s'apelle cette note? ");    
+
+    //%s pour string
+    scanf("%s", &filename);
+    
+    strcat(filename, extension);
+    fptr = fopen(filename, "w");
+
+    printf("Que voulez vous ecrire dans cette note? "); 
+    scanf("%s", &content);
+
+    fprintf(fptr, content);
+
+    fclose(fptr);
+}
+void listNotes()
+{
+
+}
+void searchNotes()
+{
+
+}
+void deleteNote()
+{
+
+}
